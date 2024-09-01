97fa8ebb0c89c80e468c8a160e8fa16be0cd9bdb
BarbosaRT <rafaelteixeira275@gmail.com>
2024-09-01 17:49:26 -0300

    Upgrade some dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:bossa/src/main_widget.dart';
 import 'package:just_audio_background/just_audio_background.dart';
